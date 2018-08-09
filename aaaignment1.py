d86c1c7f31cc786826282e1049f7072f9ec93eb5
Obi-Chidubem <42245015+Obi-Chidubem@users.noreply.github.com>
2018-08-09 20:10:53 +0000

    Add files via upload

---
+++ aaaignment1.py
@@ -0,0 +1,7 @@
+a=eval(input('enter a\n'))
+b=eval(input('enter b\n'))
+c=eval(input('enter c\n'))
+d=b*-1
+e=d+(4*a*c)
+f=e/(2*a)
+print('your answer is:',f)
---
+++ assignment2.py
@@ -0,0 +1,6 @@
+a=input('enter your name:')
+c=int(input('enter the current year:'))
+b=int(input('enter your age this year:'))
+d=100-int(b)
+e=c+d
+print(a, 'you will be 100 years old in the year',e)
